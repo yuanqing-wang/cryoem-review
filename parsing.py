abe7936190f614cfdc5d2e20aa984004917a0645
Yuanqing Wang <wangyq@wangyq.net>
2019-03-08 15:43:05 -0500

    updated parsing

--- parsing.py
+++ parsing.py
@@ -69,25 +69,27 @@ def parse_single_entry(idx: int, ftp):
     date = ""
     authors = ""
     pdb = ""
+    mol_weights = ""
+    grid_material = ""
+    grid_mesh = ""
+    grid_model = ""
+    method = ""
 
-    try:
-        # converting to entry string
-        entry = (4 - len(str(idx))) * '0' + str(idx)
-        file_name = './pub/databases/emdb/structures/EMD-%s/header/emd-%s-v30.xml' % (entry, entry)
+    # converting to entry string
+    entry = (4 - len(str(idx))) * '0' + str(idx)
+    file_name = './pub/databases/emdb/structures/EMD-%s/header/emd-%s-v30.xml' % (entry, entry)
 
-        # prepare a tempfile
-        temp_file_path = str(idx) + '_tmp.xml'
-        temp_file = open(temp_file_path, 'wb')
+    # prepare a tempfile
+    temp_file_path = str(idx) + '_tmp.xml'
+    temp_file = open(temp_file_path, 'wb')
 
-        # download
-        ftp.retrbinary('RETR ' + file_name, temp_file.write, 1024)
-        temp_file.close()
+    # download
+    ftp.retrbinary('RETR ' + file_name, temp_file.write, 1024)
+    temp_file.close()
 
-        # read the xml
-        xml_file = open(temp_file_path, 'r')
-        tree = ET.parse(xml_file)
-    except:
-        return res, doi, pubmed, date, authors, pdb
+    # read the xml
+    xml_file = open(temp_file_path, 'r')
+    tree = ET.parse(xml_file)
 
     try:
         # res
@@ -132,12 +134,160 @@ def parse_single_entry(idx: int, ftp):
     except:
         pass
 
+    # marcomolecule
+    try:
+        mols = tree.findall('./sample/supramolecule_list/' +\
+            'complex_supramolecule/molecular_weight/' +\
+            'theoretical')
+        mol_weights = ", ".join([child.text + child.attrib['units']\
+            for child in mols])
+
+    except:
+        pass
+
+    # grid
+    try:
+        grid = tree.find('.//grid')
+        model = grid.find('./model')
+        material = grid.find('./material')
+        mesh = grid.find('./mesh')
+        details = grid.find('./details')
+
+        if material != None:
+            try:
+                material = material.text
+                material = material.lower()
+
+                # standarize grid material
+                if ('copper/rhodium' in material) or\
+                    ('cu/rh' in material):
+                   grid_material = 'COPPER/RHODIUM'
+
+                elif ('copper/molybdenum' in material) or\
+                    ('cu/mo' in material):
+                    grid_material = 'COPPER/MOLYBDENUM'
+
+                elif ('cu' in material) or ('copper' in material):
+                    grid_material = 'COPPER'
+
+                elif ('gold' in material) or ('au' in material):
+                    grid_material = 'GOLD'
+
+            except:
+                pass
+
+        elif (material == None) and (details != None):
+            try:
+                material = details.text
+
+                # standarize grid material
+                if ('copper/rhodium' in material) or\
+                    ('cu/rh' in material):
+                   grid_material = 'COPPER/RHODIUM'
+
+                elif ('copper/molybdenum' in material) or\
+                    ('cu/mo' in material):
+                    grid_material = 'COPPER/MOLYBDENUM'
+
+                elif ('cu' in material) or ('copper' in material):
+                    grid_material = 'COPPER'
+
+                elif ('gold' in material) or ('au' in material):
+                    grid_material = 'GOLD'
+
+            except:
+                pass
+
+        if mesh != None:
+            try:
+                mesh = str(mesh.text)
+
+                # standarize
+                if '200' in mesh:
+                    grid_mesh = '200'
+                elif '300' in mesh:
+                    grid_mesh = '300'
+                elif '400' in mesh:
+                    grid_mesh = '400'
+
+            except:
+                pass
+
+        elif (mesh == None) and (details != None):
+            try:
+                mesh = details.text
+                # standarize
+                if '200' in mesh:
+                    grid_mesh = '200'
+                elif '300' in mesh:
+                    grid_mesh = '300'
+                elif '400' in mesh:
+                    grid_mesh = '400'
+            except:
+                pass
+
+        if model != None:
+            try:
+                model = model.text
+
+                if '1.2/1.3' in model:
+                    grid_model = 'Quantfoil R1.2/1.3'
+                elif '1/2' in model:
+                    grid_model = 'Quantfoil R1/2'
+                elif '2/2' in model:
+                    grid_model = 'Quantfoil R2/2'
+                elif '2/4' in model:
+                    grid_model = 'Quantfoil R2/4'
+                elif '2/1' in model:
+                    grid_model = 'Quantfoil R2/1'
+                elif '3/3' in model:
+                    grid_model = 'Quantfoil R3/3'
+
+            except:
+                pass
+
+        elif (model == None) and (details != None):
+            try:
+                model = details.text
+
+                if '1.2/1.3' in model:
+                    grid_model = 'Quantfoil R1.2/1.3'
+                elif '1/2' in model:
+                    grid_model = 'Quantfoil R1/2'
+                elif '2/2' in model:
+                    grid_model = 'Quantfoil R2/2'
+                elif '2/4' in model:
+                    grid_model = 'Quantfoil R2/4'
+                elif '2/1' in model:
+                    grid_model = 'Quantfoil R2/1'
+                elif '3/3' in model:
+                    grid_model = 'Quantfoil R3/3'
+            except:
+                pass
+
+    except:
+        pass
+
+
+    # method
+    try:
+        method = tree.find('./structure_determination_list/' +\
+            'structure_determination/method').text
+    except:
+        pass
+    # wrap up
     try:
         xml_file.close()
+    except:
+        pass
+
+    try:
         os.remove(temp_file_path)
     except:
         pass
-    return res, doi, pubmed, date, authors, pdb
+
+    return res, doi, pubmed, date, authors, pdb, mol_weights, grid_material,\
+        grid_mesh, grid_model, method
 
 def parse_all():
     """ Parse the whole database.
@@ -145,13 +295,18 @@ def parse_all():
     ftp = FTP('ftp.ebi.ac.uk')
     ftp.login()
 
-    df = pd.DataFrame(columns = ['index', 'doi', 'pubmed', 'date', 'authors',
-        'pdb'])
+    df = pd.DataFrame()
 
     for idx in range(1, 9784):
-        res, doi, pubmed, date, authors, pdb = parse_single_entry(idx, ftp)
-        df[idx] = [idx, res, doi, pubmed, date, authors, pdb]
-        print(idx, res, doi, pubmed, date, authors, pdb)
+        try:
+            res, doi, pubmed, date, authors, pdb, mol_weights, grid_material,\
+                grid_mesh, grid_model, method = parse_single_entry(idx, ftp)
+            df = df.append([idx, res, doi, pubmed, date, authors, pdb,
+                grid_material, grid_mesh, grid_model, method])
+            print(idx, res, doi, pubmed, date, authors, pdb, grid_material,
+                grid_mesh, grid_model, method)
+        except:
+            pass
     df.to_csv('emdb_summary.csv', sep='\t')
 
 if __name__ == '__main__':
