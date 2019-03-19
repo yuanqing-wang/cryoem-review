7ab0e5f51bc5926ee65d0d81a64e0b4707d34ee6
Yuanqing Wang <wangyq@wangyq.net>
2019-03-19 19:14:30 -0400

    single pdb

--- pdb_images/single_pdb.sh
+++ pdb_images/single_pdb.sh
@@ -1,8 +1,12 @@
 wget http://www.rcsb.org/pdb/files/$1.pdb
 mv $1.pdb rec.pdb
 count=0
-# n_cpus=$(nproc)
-n_cpus=2
+n_cpus=$(nproc)
+
+e2pdb2mrc.py rec.pdb rec.hdf --res=$res --center
+e2project3d.py rec.hdf --outfile=rec_3d.hdf --orientgen=eman:delta=1 \
+  --projector=standard --parallel=thread:$n_cpus
+  
 for ((idx=0; idx<=10; idx++)); do
 for ((res=2; res<=10; res++)); do
 for cs in 0.01 2.26; do \
@@ -12,9 +16,6 @@ for defocus in 0.5 1.0 1.5 2.0 2.5; do \
 for noiseamp in 0.1 0.2 0.3 0.4; do \
 for noiseampwhite in 0.6 0.8 1.0 1.2; do \
 for bfactor in 30, 1500; do \
-    e2pdb2mrc.py rec.pdb rec.hdf --res=$res --center
-    e2project3d.py rec.hdf --outfile=rec_3d.hdf --orientgen=eman:delta=1 \
-      --projector=standard --parallel=thread:$n_cpus
     e2proc2d.py rec_3d.hdf rec_3d_noisy.hdf \
     --process=math.simulatectf:\
       ampcont=$ampcont:\
